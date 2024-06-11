db950ed466a0e112d1710716f2db319ee7bb1ed9
Rafael Felipe <rafa_el_felipe@hotmail.com>
2024-06-11 00:04:21 -0300

    Configurado cor do tema

--- app/src/main/java/com/example/strangerquiz/ui/theme/Theme.kt
+++ app/src/main/java/com/example/strangerquiz/ui/theme/Theme.kt
@@ -16,31 +16,20 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Red80,
+    secondary = RedGrey80,
+    tertiary = RedPink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Red40,
+    secondary = RedGrey40,
+    tertiary = Red40
 )
 
 @Composable
 fun StrangerQuizTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+    darkTheme: Boolean = true,
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
